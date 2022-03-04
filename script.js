f9b949d28418ee8114c2cdb3f3ecd5508a7a8105
AllanPetersen1 <78585605+AllanPetersen1@users.noreply.github.com>
2022-03-04 12:41:42 +0100

    HTML CSS JS

---
+++ script.js
@@ -0,0 +1,29 @@
+
+ document.addEventListener("click", function(event) {
+    //Hvis der klikkes på knappen, skal der ikke ændres
+    if (event.target.closest(".btn-outline-secondary")) return;
+    //Hvis der klikkes på alt andet skal den gå tilbage til Kopier
+    if(document.getElementById('button-addon2').innerText === 'Kopieret!'){
+        document.getElementById('button-addon2').innerText = 'Kopier';
+    }
+});
+
+
+
+function copyToClipboard() {
+    /* Tager input */
+    var copyText = document.getElementById("inviteLink");
+    const button = document.getElementById("button-addon2");
+  
+    /*Markere teksten i feltet*/
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile brugere */
+  
+    /* Kopierer det til clipboarded */
+    navigator.clipboard.writeText(copyText.value);
+ 
+    /* Ændrer teksten på knappen, som confirmation */
+    document.getElementById('button-addon2').innerText = 'Kopieret!';
+  }
+
+ 
