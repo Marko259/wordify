9661a9210dc8503c3c55faa04aa3fd80ae813ae1
Markus Nielsen <animationsisnice@gmail.com>
2022-03-17 11:52:16 +0100

    Added socket.io

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
+// var socket = io.connect('http://localhost:3000');
+// socket.on('connect', function() {
+//     socket.send('Hello server from client');
 
+//     socket.on('message', function(data) {
+
+//     });
+// });
+// var express = require('express');
--- create-game.php
+++ create-game.php
@@ -13,19 +13,17 @@
 </head>
 
 <body>
-  <div class="position-absolute top-0 start-50 translate-middle-x">
-    <h1>WORDIFY</h1>
-  </div>
-
-
-  <div class="position-absolute top-50 start-50 translate-middle">
-    <div class="card text-center responsiveCard">
+   <div class="position-absolute top-50 start-50 translate-middle">
+    <div class="card text-center responsiveCard create-game-card">
       <div class="position-absolute top-0 start-0">
         <a href="index.html" id="play-btn" class="btn btn-primary mt-3 ms-3"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
       </div>
       <div class="card-body">
         <form action="" method="post">
-          <p class="card-text">Ord Længde</p>
+          <div align="center">
+          <p class="card-text w-25">Ordlængde</p>
+          </div>
+          
 
           <fieldset class="center" data-quantity="">
             <legend>Change quantity</legend>
@@ -41,12 +39,12 @@
 
           <p class="card-text-inv">Indtast selvvalgt brugernavn</p>
           <div class="form-group mb-3">
-              <input type="text" name="player_1" class="form-control" placeholder="Indtast selvvalgt brugernavn">
+              <input type="text" name="player_1" class="form-control" placeholder="Brugernavn" maxlength="16">
           </div>
 
           <p class="card-text-inv">Kode til invitation</p>
           <div class="input-group mb-3 inviteLink">
-            <input type="text" id="inviteLink" class="form-control" placeholder="Kode til modstander" aria-label="Kode til modstander" aria-describedby="button-addon2" value="QWMERM12">
+            <input type="text" id="inviteLink" class="form-control" placeholder="Kode til modstander" aria-label="Kode til modstander" aria-describedby="button-addon2" value="QWMERM12" readonly>
             <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i id="copy-btn" class="fa-regular fa-clone"></i></button>
 
           </div>
@@ -54,25 +52,19 @@
         </form>
       </div>
       <div class="card-footer text-muted">
-        <a href="">Hjælp</a>
+        <a data-bs-toggle="collapse" href="#helpCard" role="button" aria-expanded="false" aria-controls="collapseExample">Hjælp</a>
       </div>
     </div>
+    <br>
+    <div class="collapse" id="helpCard">
+  <div class="card card-body">
+    Markus er liiiidt tyk o_o
+  </div>
+</div>
   </div>
   <script src="../node_modules/jquery/dist/jquery.min.js"></script>
   <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="assets/js/script.js"></script>
-  <!--<script type="module">
-        import QuantityInput from './quantity.js';
-        (function(){
-      let quantities = document.querySelectorAll('[data-quantity]');
-    
-      if (quantities instanceof Node) quantities = [quantities];
-      if (quantities instanceof NodeList) quantities = [].slice.call(quantities);
-      if (quantities instanceof Array) {
-        quantities.forEach(div => (div.quantity = new QuantityInput(div, 'Down', 'Up')));
-      }
-    })();
-      </script>-->
 
 </body>
 
