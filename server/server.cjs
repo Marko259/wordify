3314a4febd9665422febc2e93aaeb3fdb38b1e01
Markus Nielsen <animationsisnice@gmail.com>
2022-03-26 19:51:18 +0100

    Added datetime

--- server/server.cjs
+++ server/server.cjs
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+function getDateTime() {
+  var today = new Date();
+  var date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
+  return dateTime;
+}
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log(getDateTime() + " a user connected");
+
+  socket.on("disconnect", () => {
+    console.log(getDateTime() + " user disconnected");
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 
-  socket.broadcast.emit('hi');
+  socket.broadcast.emit("hi");
 });
-
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
